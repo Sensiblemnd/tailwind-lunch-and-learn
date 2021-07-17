328ce47d4b547a5715751397f6e9186498322e69
Rob Lloveras <Sensiblemnd@users.noreply.github.com>
2021-07-17 15:19:16 -0400

    minor refactor

--- src/App.js
+++ src/App.js
@@ -1,64 +1,12 @@
 import { Component } from "react";
-
-import Logo from "./assets/altafino.svg";
 import "./app.styles.scss";
-import ctl from "@netlify/classnames-template-literals";
-
+import Card from "./components/card";
 class App extends Component {
   render() {
     return (
-      <div
-        className={ctl(`
-        flex 
-        items-center 
-        w-max
-        h-screen 
-        mx-auto`)}
-      >
-        <div
-          className={ctl(`
-        flex 
-        p-4 
-        bg-blue-500
-        rounded-lg`)}
-        >
-          <div className="flex flex-col w-2/3 pr-4 bg">
-            <p className="mb-2 text-2xl font-black text-gray-50">
-              NEXT TRIP RIAU
-            </p>
-            <p className="text-lg font-light leading-5 text-gray-300">
-              Where the earth is stepped on, there the sky is upheld
-            </p>
-            <div className="flex items-end h-full text-gray-300 hover:text-gray-50">
-              <button className="flex items-center space-x-2 text-sm font-semibold">
-                <span>BOOK NOW</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-4 h-4"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M14 5l7 7m0 0l-7 7m7-7H3"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
-          <div className="w-1/3">
-            <img
-              className="w-full rounded-lg hover:animate-bounce w"
-              src="https://picsum.photos/230/170"
-              style={{ height: "170px", width: "230px" }}
-              alt=""
-            />
-          </div>
-        </div>
-      </div>
+      <>
+        <Card />
+      </>
     );
   }
 }
---
+++ src/components/test.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function test() {
+  return <div className={"asdasd"}>;lk;lkj;lkj;lkj;lkj;lkj;ljk</div>;
+}
+
+export default test;

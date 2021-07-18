a355849d3b6cd12a7ea4a2abd3d0d238634be164
Rob Lloveras <Sensiblemnd@users.noreply.github.com>
2021-07-18 19:25:37 -0400

    Setting up @apply

--- src/components/test.js
+++ src/components/test.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function test() {
-  return <div className={"asdasd"}>;lk;lkj;lkj;lkj;lkj;lkj;ljk</div>;
+  return <div className={"test"}>test</div>;
 }
 
 export default test;

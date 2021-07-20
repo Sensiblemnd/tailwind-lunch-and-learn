eae09aceac3c661e94bbc3b89666c890a47e07bc
Rob Lloveras <Sensiblemnd@users.noreply.github.com>
2021-07-19 21:06:28 -0400

    added border

--- src/components/grids.js
+++ src/components/grids.js
@@ -2,15 +2,15 @@ import React from "react";
 
 function Grids() {
   return (
-    <div className="grid grid-cols-1 gap-5 border border-solid sm:grid-cols-2 sm:px-2 sm:py-2 sm:gap-x-8 md:py-16 md:grid-cols-3">
+    <div className="grid grid-cols-1 gap-3 border border-solid sm:grid-cols-2 sm:px-2 sm:py-2 sm:gap-5 md:py-16 md:grid-cols-3">
       <div className="p-5 text-white bg-gray-400">Col 1</div>
-      <div className="p-5">Col2</div>
+      <div className="p-5 border">Col2</div>
       <div className="p-5 text-white bg-gray-400">Col 3</div>
-      <div className="p-5">Col 1</div>
+      <div className="p-5 border">Col 1</div>
       <div className="p-5 text-white bg-gray-400">Col2</div>
-      <div className="p-5">Col 3</div>
+      <div className="p-5 border">Col 3</div>
       <div className="p-5 text-white bg-gray-400">Col 1</div>
-      <div className="p-5">Col2</div>
+      <div className="p-5 border">Col2</div>
       <div className="p-5 text-white bg-gray-400">Col 3</div>
     </div>
   );
